95296d3f3c118da9b5b2c1ba93dd234335b3d2b8
Arsalan Lodhi <alodhi@fieldtrend.com>
2025-04-17 12:38:56 -0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Arsalan Lodhi on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
